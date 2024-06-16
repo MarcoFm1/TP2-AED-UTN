040a74c9e5b2201329609b313fe4ad5b4a6a163c
Salonia04 <agusalonia04@gmail.com>
2024-06-16 10:29:44 -0300

    tu vieja

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-envios = open("envios25.txt")
-print(envios.read())
-
+'''
 pais = None
 provincia = None
 precio_inicial = None
@@ -126,6 +124,73 @@ if pago == 1:
 else:
     true_count = len(envios) - 1 
     false_count = 0
+'''
+
+envios = open("envios100HC.txt", "r", encoding="utf-8").readlines()
+#1
+def control():
+    for line in envios:
+        if "HC" in line.upper():  
+            return "Hard Control"
+        if "SC" in line.upper():  
+            return "Soft Control"
+
+
+#2
+def validation(envio: str):
+    print("-------")
+    print(f"viendo: {envio[8:29].strip()}")
+    is_mayus = False
+    some_word_is_numeric = False
+    actual_word = ""
+
+    for letter in envio[8:29].strip():
+        if letter.isalpha() or letter.isnumeric() or letter == " " or letter == ".":
+            pass
+        else:
+            return False
+
+        if letter.isalpha():
+            if letter.isupper():
+                if is_mayus:
+                    return False
+                is_mayus = True
+            else:
+                is_mayus = False
+
+        if not some_word_is_numeric:
+            if letter.isalpha() or letter.isnumeric():
+                actual_word += letter
+            else:
+                if actual_word.isnumeric():
+                    some_word_is_numeric = True
+                actual_word = ""
+        
+        if actual_word.isnumeric():
+            some_word_is_numeric = True
+        
+    if some_word_is_numeric:
+        return True
+    return False
+
+true_count = 0
+false_count = 0
+
+for envio in envios[1:]:
+    result = validation(envio)
+    print(f"{result}")
+    
+    if result:
+        true_count += 1
+    else:
+        false_count += 1
+
+if control() == "Hard Control":
+    print(f"Total Valid: {true_count}")
+    print(f"Total Invalid: {false_count}")
+else:
+    true_count = len(envios) - 1 
+    false_count = 0
 
 
 
@@ -186,4 +251,7 @@ print('(r11) - Importe menor pagado por envios a Brasil:', menimp())
 print('(r12) - Codigo postal del envio a Brasil con importe menor:', mencp())
 print('(r13) - Porcentaje de envios al exterior sobre el total:', porc())
 print('(r14) - Importe final promedio de los envios a Buenos Aires:', prom())
+
+
+
 
